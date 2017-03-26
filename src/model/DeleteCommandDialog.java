7c04e5a5ab942582cf0485bfbb0ecf9054ff37f6
Your Name <you@example.com>
2017-03-26 21:06:56 +0200

    works on gui

---
+++ src/model/DeleteCommandDialog.java
@@ -0,0 +1,44 @@
+package model;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.UIManager;
+
+public class DeleteCommandDialog extends JDialog {
+
+	private static final long serialVersionUID = 2019784532217781110L;
+	JLabel deleteText;
+	JButton confirmButton;
+	JButton cancelButton;
+
+	public DeleteCommandDialog() {
+
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		deleteText = new JLabel("Do you really want to delete this command?");
+		confirmButton = new JButton("Delete");
+		cancelButton = new JButton("Cancel");
+
+		this.add(deleteText);
+		this.add(confirmButton);
+		this.add(cancelButton);
+
+		deleteText.setBounds(40, 25, 400, 30);
+		confirmButton.setBounds(80, 90, 100, 30);
+		cancelButton.setBounds(210, 90, 100, 30);
+
+		this.setLayout(null);
+		this.setVisible(true);
+		this.setSize(420, 200);
+	}
+
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		DeleteCommandDialog window = new DeleteCommandDialog();
+	}
+}
--- src/model/Gui.java
+++ src/model/Gui.java
@@ -7,14 +7,34 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 import javax.swing.UIManager;
 
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+//import javax.swing.tree.TreeNode;
+
 public class Gui {
 
-	JFrame frame;
+	JFrame frame = new JFrame("voice2key");
+
+	// command tree
+	JScrollPane scrollPane;
+
 	JButton startListeningButton;
 	JButton stopListeningButton;
+
 	JButton createCommandButton;
-	JTextField textField;
-	Sphinx4Runner listener = new Sphinx4Runner();
+	JButton deleteCommandButton;
+
+	//
+	JLabel commandLabel;
+	JLabel urlLabel;
+	JTextField commandTextField;
+	JTextField urlTextField;
+	JButton confirmCommandButton;
+	JButton cancelCommandButton;
+
+	Sphinx4Runner listener;
 
 	public Gui() {
 
@@ -24,56 +44,113 @@ public class Gui {
 			e.printStackTrace();
 		}
 
-		frame = new JFrame("voice2key");
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode("Wurzel");
+		JTree tree = new JTree(root);
+
+		commandLabel = new JLabel("Command:");
+		urlLabel = new JLabel("URL:");
+		commandTextField = new JTextField();
+		urlTextField = new JTextField();
+
+		scrollPane = new JScrollPane(tree);
 		startListeningButton = new JButton("Start Listening");
 		stopListeningButton = new JButton("Stop Listening");
-		createCommandButton = new JButton("create command");
-		textField = new JTextField();
+		createCommandButton = new JButton("create new command");
+		deleteCommandButton = new JButton("delete command");
+		confirmCommandButton = new JButton("Enter");
+		cancelCommandButton = new JButton("Cancel");
 
 		layout();
 		actions();
-
 	}
 
 	public void layout() {
+		frame.add(scrollPane);
+		frame.add(commandLabel);
+		frame.add(urlLabel);
 		frame.add(startListeningButton);
 		frame.add(stopListeningButton);
 		frame.add(createCommandButton);
-		frame.add(textField);
+		frame.add(commandTextField);
+		frame.add(urlTextField);
+		frame.add(deleteCommandButton);
+		frame.add(createCommandButton);
+		frame.add(confirmCommandButton);
+		frame.add(cancelCommandButton);
 		frame.setLayout(null);
 		frame.setVisible(true);
 		frame.setSize(800, 600);
-		textField.setBounds(50, 50, 200, 20);
+		scrollPane.setBounds(30, 20, 300, 300);
+
+		createCommandButton.setBounds(360, 20, 200, 40);
+		deleteCommandButton.setBounds(360, 80, 200, 40);
+
+		commandLabel.setBounds(360, 130, 400, 30);
+		commandTextField.setBounds(360, 160, 320, 30);
+		urlLabel.setBounds(360, 200, 400, 30);
+		urlTextField.setBounds(360, 230, 320, 30);
+		confirmCommandButton.setBounds(360, 280, 150, 40);
+		cancelCommandButton.setBounds(530, 280, 150, 40);
+
 		startListeningButton.setBounds(30, 480, 150, 40);
 		stopListeningButton.setBounds(210, 480, 150, 40);
+
+		commandLabel.setVisible(false);
+		urlLabel.setVisible(false);
+		commandTextField.setVisible(false);
+		urlTextField.setVisible(false);
+		confirmCommandButton.setVisible(false);
+		cancelCommandButton.setVisible(false);
 
 	}
 
 	public void actions() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ActionListener createButtonListener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				commandLabel.setVisible(true);
+				urlLabel.setVisible(true);
+				commandTextField.setVisible(true);
+				urlTextField.setVisible(true);
+				confirmCommandButton.setVisible(true);
+				cancelCommandButton.setVisible(true);
+			}
+		};
+		ActionListener cancelButtonListener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				commandLabel.setVisible(false);
+				urlLabel.setVisible(false);
+				commandTextField.setVisible(false);
+				urlTextField.setVisible(false);
+				confirmCommandButton.setVisible(false);
+				cancelCommandButton.setVisible(false);
+			}
+		};
 		ActionListener startButtonListener = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				textField.setText("b1");
+				listener = new Sphinx4Runner();
 				listener.startSpeechThread();
 			}
 		};
 		ActionListener stopButtonListener = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				textField.setText("b2");
 				listener.stopSpeechThread();
 			}
 		};
 
+		createCommandButton.addActionListener(createButtonListener);
+		cancelCommandButton.addActionListener(cancelButtonListener);
 		startListeningButton.addActionListener(startButtonListener);
 		stopListeningButton.addActionListener(stopButtonListener);
 	}
 
 	public static void main(String[] args) {
-
 		@SuppressWarnings("unused")
 		Gui window = new Gui();
-
 	}
 
 }
